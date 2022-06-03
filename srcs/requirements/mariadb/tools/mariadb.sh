4aef6d12acd8794580edde24dfb0a8a05770b142
VLN37 <jofelipe@student.42sp.org.br>
2022-06-03 13:58:38 -0300

    fix: automatic alternate user creation in mariadb

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 service mysql start
-# echo "123\n n\n y\n n\n n\n y\n" | mysql_secure_installation
+echo "123\n n\n y\n n\n n\n y\n" | mysql_secure_installation
+mysql -e "create user 'root'@'%' identified by '123';FLUSH PRIVILEGES"
 service mysql stop
 # touch log
 # mysqld --skip-syslog --log-error=log
