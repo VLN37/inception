33db9329d5268c75bb7ca871154ceaed9ba4d75f
VLN37 <jofelipe@student.42sp.org.br>
2022-06-03 13:26:36 -0300

    feat: set up a working network between services

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 service mysql start
-echo "123\n n\n y\n n\n n\n y\n" | mysql_secure_installation
+# echo "123\n n\n y\n n\n n\n y\n" | mysql_secure_installation
 service mysql stop
 # touch log
 # mysqld --skip-syslog --log-error=log
