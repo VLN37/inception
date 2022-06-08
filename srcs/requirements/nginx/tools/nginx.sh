fbde8fd1fc8c4c6f2734f49a8c70c681594eb380
VLN37 <jofelipe@student.42sp.org.br>
2022-06-07 23:22:52 -0300

    feat: webserver now works with SSL

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -2,9 +2,24 @@
 
 echo "$@"
 
+# export CERT="123"
+# export CERT_KEY="123"
+service openssl start
+openssl req -new -nodes -x509 \
+-newkey rsa:4096 \
+-days 180 \
+-keyout $CERT_KEY \
+-out $CERT \
+-subj "/C=BR/ST=SP/L=SP/O=42/OU=jofelipe/CN=jofelipe.42.fr"
+service openssl stop
+
+sed -i "s#PLACEHOLDER_CERT_KEY#$CERT_KEY#"\
+       /etc/nginx/sites-available/server1.conf
+sed -i "s#PLACEHOLDER_CERT#$CERT#" /etc/nginx/sites-available/server1.conf
+
 # mv /config /etc/nginx/sites-available/
 #config
 # service nginx start
 # rm /etc/nginx/sites-available/default
 
-exec "$@"
+# exec "$@"
