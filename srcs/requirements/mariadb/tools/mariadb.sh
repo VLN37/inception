12f23d9b7d673057a09f9a7c93c63784bbd399af
VLN37 <jofelipe@student.42sp.org.br>
2022-06-08 00:19:36 -0300

    fix: mariadb not asking for root password

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 service mysql start
-echo "123\n n\n y\n n\n n\n y\n" | mysql_secure_installation
-mysql -e "create user 'root'@'%' identified by '123';FLUSH PRIVILEGES"
+mysql -e "create database wordpress"
+mysql -e "update user set plugin='' where User='root'; FLUSH PRIVILEGES"
+echo "123\n n\n y\n y\n y\n y\n" | mysql_secure_installation
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123'; FLUSH PRIVILEGES"
+mysql -e "CREATE USER 'root'@'%' identified by '123';FLUSH PRIVILEGES"
 service mysql stop
 # touch log
 # mysqld --skip-syslog --log-error=log
