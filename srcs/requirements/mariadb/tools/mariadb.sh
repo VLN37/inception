5bd5d6f37e0b0ae39a64972d5b7c06dff1b8b358
VLN37 <jofelipe@student.42sp.org.br>
2022-06-08 15:44:04 -0300

    fix: mariadb script properly creates user

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -2,9 +2,10 @@
 
 service mysql start
 mysql -e "create database wordpress"
-mysql -e "update user set plugin='' where User='root'; FLUSH PRIVILEGES"
+mysql -e "update user set plugin='' where User='root'; FLUSH PRIVILEGES;"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123'; FLUSH PRIVILEGES;"
+mysql -e "CREATE USER 'root'@'%' identified by '123';FLUSH PRIVILEGES;"
+mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'%';"
 echo "123\n n\n y\n y\n y\n y\n" | mysql_secure_installation
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123'; FLUSH PRIVILEGES"
-mysql -e "CREATE USER 'root'@'%' identified by '123';FLUSH PRIVILEGES"
 service mysql stop
 exec "$@"
