8e0012019aaa9d242c4ce54652431fbe99c3cd54
VLN37 <jofelipe@student.42sp.org.br>
2022-06-09 20:38:57 -0300

    fix: change configuration of database volume

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 service mysql start
-mysql -e "create database wordpress"
-mysql -e "update user set plugin='' where User='root'; FLUSH PRIVILEGES;"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123'; FLUSH PRIVILEGES;"
+mysql -e "create database wordpress;"
+# mysql -e "update user set plugin='' where User='root'; FLUSH PRIVILEGES;"
 mysql -e "CREATE USER 'root'@'%' identified by '123';FLUSH PRIVILEGES;"
 mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'%';"
-echo "123\n n\n y\n y\n y\n y\n" | mysql_secure_installation
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123'; FLUSH PRIVILEGES;"
+# echo "123\n n\n y\n y\n y\n y\n" | mysql_secure_installation
 service mysql stop
 exec "$@"
